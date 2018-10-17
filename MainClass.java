408f671ed88f4fe7ca21e87e0e975e12ca5f75b2
zzzjs <zzzjs@users.noreply.github.com>
2018-10-16 21:02:42 -0400

    test
    1

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		for (int i = 0; i < 2; i++) {
+			System.out.println("Hello World again!");
+		}
+	}
+}
